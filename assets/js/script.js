61f758902a197516c076926752412e87342edf32
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-12-01 15:56:58 +0100

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const today = new Date();
+const currentDay = today.getDate() + 2; // 1 декабря → 1
+
+document.querySelectorAll(".gift-box").forEach((box) => {
+  const day = parseInt(box.dataset.day);
+
+  if (day <= currentDay) {
+    // День открыт
+    box.addEventListener("click", () => {
+      window.location.href = `days/day${day}.html`;
+    });
+  } else {
+    // День ещё закрыт
+    box.classList.add("gift-disabled");
+  }
+});
